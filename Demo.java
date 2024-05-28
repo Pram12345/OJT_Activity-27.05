72992408fd35e275fdad196e42050a8f40bfde8f
Ramakant Prajapati <121657019+Pram12345@users.noreply.github.com>
2024-05-28 09:39:56 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        Student st1;
+        st1 = new Student();
+        st1.StudentId = 123;
+        st1.StudentName = "Ramakant";
+        st1.StudentCity = "Dehradun";
+        st1.study();
+        st1.showfullDetails();
+    }
+}
---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    int StudentId;
+    String StudentName;
+    String StudentCity;
+
+
+    public void study(){
+        System.out.println(StudentName + "is studing");
+    }
+
+    public void showfullDetails(){
+        System.out.println("My name is " +StudentName);
+        System.out.println("My id is " +StudentId);
+        System.out.println("My city is " +StudentCity);
+    }
+
+    
+}
+
+
